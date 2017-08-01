4a20f6cdce542bc926302c516ec5190283e691be
Andrews <nick.andrews@tylertech.com>
2017-08-01 15:58:21 -0400

    updated mpl.js

--- mpl.js
+++ mpl.js
@@ -30,37 +30,6 @@ var handlebars = require('express-handlebars')
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
-
-function exposeTemplates(req, res, next) {
-    // Uses the `ExpressHandlebars` instance to get the get the **precompiled**
-    // templates which will be shared with the client-side of the app.
-    hbs.getTemplates('views/layouts/', {
-        cache      : app.enabled('view cache'),
-        precompiled: true
-    }).then(function (templates) {
-        // RegExp to remove the ".handlebars" extension from the template names.
-        var extRegex = new RegExp(hbs.extname + '$');
-
-        // Creates an array of templates which are exposed via
-        // `res.locals.templates`.
-        templates = Object.keys(templates).map(function (name) {
-            return {
-                name    : name.replace(extRegex, ''),
-                template: templates[name]
-            };
-        });
-
-        // Exposes the templates during view rendering.
-        if (templates.length) {
-            res.locals.templates = templates;
-        }
-
-        setImmediate(next);
-    })
-    .catch(next);
-}
-
-
 //set port number
 app.set('port', process.env.PORT || 3000);
 
